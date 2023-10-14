7219d4e85a961d97b9737096a14c17ded10774a1
Elvis Njau <elvisbater@gmail.com>
2023-10-14 18:02:23 -0300

    declared all the variable needed for this code challenged

---
+++ index.css
@@ -0,0 +1,6 @@
+const CHARACTERS_URL = "http://localhost:3000/characters";
+const animalList = document.getElementById("animals-list");
+const animalDetailsContainer = document.getElementById("animal-details");
+const voteButton = document.getElementById("vote-button");
+const resetButton = document.getElementById("reset-button");
+const addAnimalForm = document.getElementById("add-animal-form");
